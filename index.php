8a5eb33671f4e161f57846ec812430601cc5aedb
MaysamTurk <71770879+MaysamTurk@users.noreply.github.com>
2021-01-20 01:32:45 +0200

    php logical statement tasks

---
+++ index.php
@@ -0,0 +1,128 @@
+<head>
+	<title>Document</title>
+    <style>
+    .style  {
+        background: pink;
+    }
+    </style>
+</head>
+ 
+ <!-- Question 1 -->
+
+ <?php 
+//  $a = 2.5;
+//  $b = 5;
+//  $c = 6.2;
+//  $d = 7.5;
+
+//  $Units = 100; // unit amount
+//  $Bill;
+
+//  if ($Units <= 50 ){
+//       $Bill = ($Units * $a) ;
+
+//  } 
+//  elseif($Units <= 150) {
+//     $Bill = ($Units * $b);
+
+//  }
+//  elseif($Units <= 250) {
+//     $Bill = ($Units * $c);
+
+//  }
+//  else ($Units >= 250) {
+//     $Bill = ($Units * $d)
+
+//  };
+//  echo $Bill;
+
+ 
+ ?>
+
+<?php
+//Question 2--------------------------------
+
+// $first_num = $P['first_num'];
+// $second_num = $P['second_num'];
+// $operator = $P['operator'];
+// $result = '';
+// if (is_numeric($first_num) && is_numeric($second_num)) {
+//     switch ($operator) {
+//         case "Add":
+//            $result = $first_num + $second_num;
+//             break;
+//         case "Subtract":
+//            $result = $first_num - $second_num;
+//             break;
+//         case "Multiply":
+//             $result = $first_num * $second_num;
+//             break;
+//         case "Divide":
+//             $result = $first_num / $second_num;
+//     }
+// }
+// Please unComment the html below
+
+//  Question 3---------------------
+// function voting() 
+// {
+//     $age = 20;
+//     if ($age >= 18) {
+//         echo "You can Vote";
+//     } else {
+//         echo " You can not Vote because your age is less than 18 ";
+//     }
+// }
+// voting(); 
+
+// Question 4 --------------------------
+
+// $num = -4;
+// if ($num > 0){
+//     echo "Positive";
+//     } elseif 
+//     ($num < 0){
+//         echo "Negative";
+//     } elseif 
+//     ($num === 0){
+//         echo "Zero";
+//     }
+//     elseif (!is_numeric($num)) {
+//         echo "Please input only number";
+//     }
+?>
+<!-- 
+<body>
+    <div id="page-wrap">
+	<h1>Calculator</h1>
+	  <form action="" method="post" id="quiz-form">
+            <p>
+                <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>Num 1</b>
+            </p>
+            <p>
+                <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Num 2</b>
+            </p>
+            <p>
+            <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+            </p>
+            <input type="submit" name="operator" value="+" class="style" />
+            <input type="submit" name="operator" value="-" class="style"/>
+            <input type="submit" name="operator" value="×" class="style"/>
+            <input type="submit" name="operator" value="÷" class="style" />
+	  </form>
+    </div> -->
+
+
+    
+    <!-- <form action="" method="post" id="quiz-form">
+            	<input type="number" name="units" id="units" placeholder="Please enter no. of Units" />
+            	<input type="submit" name="unit-submit" id="unit-submit" value="Submit" />
+		</form>
+
+		<div>
+		    <?php echo '<br />' . $Bill; ?>
+		</div> -->
+
+    
+</body>
+</html>
